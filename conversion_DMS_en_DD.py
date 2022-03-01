0568c82fed441f7afa2345ea4654d27d192bc077
Imoen148 <100290514+Imoen148@users.noreply.github.com>
2022-03-01 13:53:22 -0500

    Update conversion_DMS_en_DD.py

--- conversion_DMS_en_DD.py
+++ conversion_DMS_en_DD.py
@@ -11,18 +11,17 @@ def conv_DMS_DD(lattitude_dms, longitude_dms):
 
     return dd_la, dd_lo
 
-def distance_pole_nord (lat_1, long_1, lat_2, long_2):
+def distance_pole_nord (coordonnee_1, coordonnée_2):
     # calcul distance entre 2 coordonée = racine carré de ((x2-x1)2 + (y2-y1)2)  
+    lat_1, long_1 = coordonnee_1
+    lat_2, long_2 = coordonnée_2
     distance = math.sqrt(((lat_2 - lat_1)**2 + (long_2 - long_1)**2))
     return distance
 
-LAT_POLE_N = 86
-LONG_POLE_N = 172
+COORDONNEE_POLE_NORD = 86, 172
 lattitude_dms_user = 48, 51, 23.81
 longitude_dms_user = 2, 21, 7.9999
+coordonee_user = conv_DMS_DD(lattitude_dms_user, longitude_dms_user)
 
-lat_u, long_u = conv_DMS_DD(lattitude_dms_user, longitude_dms_user)
-
-print(lat_u)
-print(long_u)
-print(distance_pole_nord(LAT_POLE_N, LONG_POLE_N, lat_u, long_u))
+print(coordonee_user)
+print(distance_pole_nord(COORDONNEE_POLE_NORD, coordonee_user))
