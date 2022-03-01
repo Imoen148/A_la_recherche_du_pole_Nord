7a882c4baebda0168122672c297b3f75ec3f11d1
Imoen148 <100290514+Imoen148@users.noreply.github.com>
2022-03-01 10:53:56 -0500

    Update conversion_DMS_en_DD.py

--- conversion_DMS_en_DD.py
+++ conversion_DMS_en_DD.py
@@ -1,13 +1,13 @@
-from math import *
+import math
 
 def conv_DMS_DD(degLa, minLa, secLa, degLo, minLo, secLo):
     DdLa = degLa + (minLa/60) + (secLa/3600)
     DdLo = degLo + (minLo/60) + (secLo/3600)
     return DdLa, DdLo
 
-def distance_pole_nord (Lat1, Long1, lat2, long2):
- # racine carré de ((x2-x1)2 +(y2-y1)2)  
-    distance = sqrt(((Lat1 - Long1)**2 + (lat2 - long2)**2))
+def distance_pole_nord (lat1, long1, lat2, long2):
+ # racine carré de ((x2-x1)2 + (y2-y1)2)  
+    distance = math.sqrt(((lat2 - lat1)**2 + (long2 - long1)**2))
     return distance
 
 # Coordonée du pole nord, Lat : 81.3  Long : 110.8
